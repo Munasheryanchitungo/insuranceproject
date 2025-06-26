22842283fc4fa5df197737c41a341f76cbce944c
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-26 20:32:35 +0200

    fix: correct users app reference in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = [
     'jazzmin',
+    'backend.users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,8 +1,6 @@
-from django.apps import AppConfig
-
-
+# backend/users/apps.py
 from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'  # Make sure this matches exactly
+    name = 'backend.users'
