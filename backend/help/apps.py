aa18f3d70d69c823ec744aa2b0a9262c7020a8f6
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-27 10:33:06 +0200

    fix: correct users app import path

--- backend/help/apps.py
+++ backend/help/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class HelpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'help'  # Must match exactly with your app name
+    name = 'backend.help'  # Must match exactly with your app name
