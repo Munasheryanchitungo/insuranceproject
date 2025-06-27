df36420c9004773f6833e66c7ba41797d8a18733
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-27 10:23:54 +0200

    fix: correct users app import path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = [
     'jazzmin',
-    'backend.users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
