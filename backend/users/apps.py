a69f12fb68d3942ef193da0cde4db2774b3b09ef
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-27 15:48:40 +0200

    fix: correct users app import path

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'  # Must match exactly (no 'backend.')
+    name = 'backend.users'  # Must match exactly (no 'backend.')
