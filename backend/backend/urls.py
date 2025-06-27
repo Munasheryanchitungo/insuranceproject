456bf8afedf7ddf352c12460298b3f7505bf1ad2
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-27 10:43:12 +0200

    fix: correct users app import path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('policies/', include('policies.urls')),
-    path('claims/', include('claims.urls')),
-    path('payments/', include('payments.urls')),
-    path('help/', include('help.urls')),
+    path('policies/', include('backend.policies.urls')),
+    path('claims/', include('backend.claims.urls')),
+    path('payments/', include('backend.payments.urls')),
+    path('help/', include('backend.help.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
