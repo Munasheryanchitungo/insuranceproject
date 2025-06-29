70d839760dfb97e139e04a8129d912264569ee7e
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-29 13:51:29 +0200

    Add temporary createsuperuser route

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = [
     'jazzmin',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
