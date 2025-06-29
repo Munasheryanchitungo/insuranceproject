57139314d50f01e28c5b9179418995a8b07de93f
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-06-29 14:00:28 +0200

    Add temporary createsuperuser route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,7 +13,6 @@ def create_superuser(request):
     return HttpResponse("Superuser already exists")
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('policies/', include('policies.urls')),
