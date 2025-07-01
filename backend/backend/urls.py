bcea48f973e79aafec0da4770b49fd5f9ed2ffd0
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-07-01 16:29:24 +0200

    my final

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
-from . import views
+
 
 def create_superuser(request):
     User = get_user_model()
