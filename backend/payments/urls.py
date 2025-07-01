eb347b2bf758d3158fc1eb6f34b3bf3f19ee79e4
Munashe Ryan Chitungo <munasheryanchitungo@gmail.com>
2025-07-01 17:58:42 +0200

    my final

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('process/', views.process_payment, name='process_payment'),
+    path('api/process_payment/', views.process_payment_api, name='process_payment_api'),
+
 ]
